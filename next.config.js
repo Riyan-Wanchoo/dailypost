93628a54d36a6bd6fd690e55e1ca7eab89cc6ea2
Riyan <riyanwanchoo1072008@yahoo.com>
2023-04-06 15:29:18 +0530

    new api token

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
 module.exports = {
   env: {
     apiHost: 'https://dailypost-backend-production.up.railway.app',
-    publicAccessToken: "8defe4cbc082b18e691b01a032074456caae8b9dea8137e1b61c87d6a37b29aa44e462aa132c68ff05d50514c7d0e1a13389bd9580176f1e92dc1eb648f75509a3e1e13dd7381c0e827bb0c32ad491bd838d9c12d2ea518e838a28f35c52c3679042735e24d94a6816d695892328563ae6644803ac08fdc48a5301856e19be6c"
+    publicAccessToken: "e2f7501cc2375ccab2ce1cf698cb24a6ada4576a6e5ad0eab89c24f279d4b5f468aec0ce96ac3881f1bdddefe76d89a45df3d9216b07bf0466d8dc6b2cff3a7a54cf1ff3f8acdd0ef98fbd2e4706b9bf179ba340899586ae648d2dcf2512851bbc6ba7b2488e71b68c1f041e7659ea334daf8d602c1c06c9ecdf7f7bce0c3a44"
   },
   images: {
     domains: ['dailypost-backend-production.up.railway.app']
